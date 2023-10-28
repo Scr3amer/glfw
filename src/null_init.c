83e22f2227a662010700375bc23441d16b484762
Scr3amer <Scr3amDev@outlook.com>
2023-10-28 16:38:35 -0400

    Add null changes

--- src/null_init.c
+++ src/null_init.c
@@ -90,6 +90,8 @@ GLFWbool _glfwConnectNull(int platformID, _GLFWplatform* platform)
         _glfwHideWindowNull,
         _glfwRequestWindowAttentionNull,
         _glfwFocusWindowNull,
+        _glfwDragWindowNull,
+        _glfwResizeWindowNull,
         _glfwSetWindowMonitorNull,
         _glfwWindowFocusedNull,
         _glfwWindowIconifiedNull,
